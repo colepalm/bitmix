2c46cfd1804afbbc686425dd8f391245a382ff36
colepalm <palm.cole@gmail.com>
2017-06-30 00:57:34 -0700

    First steps for UI

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-# `angular-seed` — the seed for AngularJS apps
-
-This project is an application skeleton for a typical [AngularJS][angularjs] web app. You can use it
-to quickly bootstrap your angular webapp projects and dev environment for these projects.
-
-The seed contains a sample AngularJS application and is preconfigured to install the Angular
-framework and a bunch of development and testing tools for instant web development gratification.
-
-The seed app doesn't do much, just shows how to wire two controllers and views together.
-
+# `bitmixer` — a sample bitcoin mixer using jobcoin
 
 ## Getting Started
 
-To get you started you can simply clone the `angular-seed` repository and install the dependencies:
+To get you started you can simply clone the `bitmix` repository and run `npm run`:
 
 ### Prerequisites
 
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,17 +10,39 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [
-    '$scope', '$http',
-    function($scope, $http) {
-    $scope.getData = function () {
-        $http({
-            method: 'GET',
-            url: 'http://jobcoin.projecticeland.net/incumber/api/addresses/1'
-        }).then(function successCallback(response) {
-            console.log(response);
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    };
+    '$scope', '$http', function($scope, $http) {
+        $scope.addresses = [];
+        $scope.addresses[0] = {};
+
+        function init() {
+            $http({
+                method: 'GET',
+                url: 'http://jobcoin.projecticeland.net/incumber/api/addresses/1'
+            }).then(function successCallback(response) {
+                $scope.bank = response.data.balance;
+            }, function errorCallback(response) {
+                console.warn(response);
+            });
+
+            $http({
+                method: 'GET',
+                url: 'http://jobcoin.projecticeland.net/incumber/api/addresses/2'
+            }).then(function successCallback(response) {
+                $scope.userCoins = response.data.balance;
+            }, function errorCallback(response) {
+                console.warn(response);
+            });
+        }
+
+        init();
+
+        $scope.mixCoins = function () {
+
+        };
+
+        $scope.handleRowChange = function (index) {
+            if ($scope.addresses[index].address > 0 && !$scope.address[index+1]) {
+                $scope.addresses[index+1] = {};
+            }
+        }
 }]);
