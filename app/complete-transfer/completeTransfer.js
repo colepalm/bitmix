dd691e85c1ffebb51b56514dc2dd8c6c70d53884
colepalm <palm.cole@gmail.com>
2017-07-10 12:36:40 -0600

    Final

--- app/complete-transfer/completeTransfer.js
+++ app/complete-transfer/completeTransfer.js
@@ -4,40 +4,40 @@ angular.module('myApp.completeTransfer', ['ngRoute'])
 
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/complete-transfer', {
-      templateUrl: 'complete-transfer/complete-tranfer.html',
+      templateUrl: 'complete-transfer/complete-transfer.html',
       controller: 'completeTransferCtrl'
     });
   }])
 
-  .controller('completeTransferCtrl', [
-    '$scope', '$http', function($scope, $http) {
-      $scope.accounts = [];
+.controller('completeTransferCtrl', [
+  '$scope', '$http', function($scope, $http) {
+    $scope.accounts = [];
 
-      //populate bank with account #1
-      function init() {
-        var toParse = localStorage.getItem('recieveNumbers');
+    //populate bank with account #1
+    function init() {
+      var toParse = localStorage.getItem('recieveNumbers');
 
-        $scope.accounts = parseAccountNumbers(toParse);
+      $scope.accounts = parseAccountNumbers(toParse);
 
-        fillAccounts(accountNumbers);
-      }
-      init();
+      fillAccounts($scope.accounts);
+    }
+    init();
 
-      function fillAccounts(accountNumbers) {
-        accountNumbers.forEach(function(num, index) {
-          $http({
-            method: 'GET',
-            url: 'http://jobcoin.projecticeland.net/incumber/api/addresses/' + num
-          }).then(function successCallback(response) {
-            $scope.accounts[index] = {};
-            $scope.accounts[index].accountNumber = num;
-            $scope.accounts[index].balance = response.data.balance;
-          });
-        })
-      }
+    function fillAccounts(accountNumbers) {
+      accountNumbers.forEach(function(num, index) {
+        $http({
+          method: 'GET',
+          url: 'http://jobcoin.projecticeland.net/incumber/api/addresses/' + num
+        }).then(function successCallback(response) {
+          $scope.accounts[index] = {};
+          $scope.accounts[index].accountNumber = num;
+          $scope.accounts[index].balance = response.data.balance;
+        });
+      })
+    }
 
-      function parseAccountNumbers(nums) {
-        return nums.split(',');
-      }
+    function parseAccountNumbers(nums) {
+      return nums.split(',');
+    }
 
-    }]);
+  }]);
